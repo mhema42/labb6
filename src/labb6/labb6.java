ada7ef368be1b27777384f967b2eeb99409721e6
Mats <mhema@telia.com>
2022-09-23 10:33:49 +0200

    made a simple menu for different choice

--- src/labb6/labb6.java
+++ src/labb6/labb6.java
@@ -1,9 +1,40 @@
 package labb6;
 
-public class labb6 {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		System.out.println("test");
-
+public class labb6 
+{
+	
+	public static String menu() 
+	{
+		
+		try (Scanner input = new Scanner(System.in))
+		{
+			System.out.println("Skriv ditt namn:");
+			String name = input.nextLine();
+			
+			System.out.println("Välj alternativ:");
+			System.out.print("1 - Hejsan   2 - Hello    3 - Priviet");
+			
+			String choice = input.nextLine();
+			String text = null;
+			
+			switch (choice) 
+			{
+				case "1": text = "Hejsan " + name;
+				break;
+				case "2": text = "Hello " + name;
+				break;
+				case "3": text = "Priviet " + name;
+				break;	
+			}
+			return text;
+		}
+	}
+	
+	public static void main(String[] args) 
+	{
+		String choice = menu();
+		System.out.println(choice);
 	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,3 @@
 module test {
+	requires java.desktop;
 }
